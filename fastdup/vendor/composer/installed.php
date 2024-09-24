6d4ffd7c0c7dc1542bac034d2349f61fe7fe47ff
Sean Murphy <sean@iamseanmurphy.com>
2024-09-24 08:55:31 +0000

    2.3

---
+++ fastdup/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thanh.huynh/njt-fastdup',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '193116e2d2201f557cf4868a86caa71d743c87bc',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ifsnop/mysqldump-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2d3a43fc0c49f23bf7dee392b0dd1f8c799f89d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ifsnop/mysqldump-php',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'neitanod/forceutf8' => array(
+            'pretty_version' => 'v2.x-dev',
+            'version' => '2.9999999.9999999.9999999-dev',
+            'reference' => 'c1fbe70bfb5ad41b8ec5785056b0e308b40d4831',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../neitanod/forceutf8',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'thanh.huynh/njt-fastdup' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '193116e2d2201f557cf4868a86caa71d743c87bc',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
